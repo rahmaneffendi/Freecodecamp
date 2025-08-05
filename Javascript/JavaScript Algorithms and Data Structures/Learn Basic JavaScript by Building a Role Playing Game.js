62e38f6cd4f3806fbac2654940e24e3570a0107f
Abdul Rahman Effendi <99697182+rahmaneffendi@users.noreply.github.com>
2025-08-05 12:43:19 +0700

    step 1
    Step 1
    JavaScript is a powerful language which allows you to build websites that are interactive.
    
    Note: For all remaining projects in this curriculum, you will need a basic level of knowledge in HTML and CSS. If you are new to HTML and CSS, please go through the Responsive Web Design Certification.
    
    To get started, create your standard HTML boilerplate with a DOCTYPE, html, head, and body, then add a meta tag for the charset. Add a title element and use the text RPG - Dragon Repeller for it. Include a link tag for your stylesheet to link the styles.css file.
    
    Finally, create a div element with id set to game within your body.

---
+++ Javascript/JavaScript Algorithms and Data Structures/Learn Basic JavaScript by Building a Role Playing Game.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>RPG - Dragon Repeller</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div id="game">
+    <!-- Konten game akan ditambahkan di sini nanti -->
+  </div>
+</body>
+</html>
